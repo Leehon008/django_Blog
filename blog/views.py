dd9b84bca6657cbcfbeb37f6da29e54143979681
Leehon008 <honye.lewism@gmail.com>
2019-09-19 13:23:47 +0200

    Included some profile,login,logout fns.. semi finished version

--- blog/views.py
+++ blog/views.py
@@ -1,32 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 
-posts = [
-    {
-        'author' : 'Lewis HM',
-        'title' : 'How to be Involved',
-        'content' : 'Dummy status yacho',
-        'posted_on': '21 August 2019'
-    },
-    {
-        'author': 'Hon HM',
-        'title': 'How to be Independent',
-        'content': 'Relation status yacho',
-        'posted_on': '21 June 2015'
-    },
-    {
-        'author': 'Leer oy ',
-        'title': 'Anonymously Involved',
-        'content': 'Restricted',
-        'posted_on': '12 Dec 2009'
-    }
-
-
-]
 def home(request):
     context = {
-        'posts' : posts
+        'posts': Post.objects.all()
     }
     return render(request,'blog/home.html', context)
 
 def about(request):
-    return render(request, 'blog/about.html', {'title': 'About'})
+    return render(request,'blog/about.html',{'title':'About'})
--- djangoBlogApp/settings.py
+++ djangoBlogApp/settings.py
@@ -33,11 +33,13 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
+    'crispy_forms',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = 'blog-home'
+
+LOGIN_URL = 'login'
