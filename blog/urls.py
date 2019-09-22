1b62251419feb6825a674bdd4e071c6cffc5ab6e
Leehon008 <honye.lewism@gmail.com>
2019-09-22 10:38:44 +0200

    new additions close to finish

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from . import views
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, UserPostListView
 
 urlpatterns = [
-    path('', views.home, name='blog-home'),
-    path('about/', views.about, name='blog-about'),
+    path('', PostListView.as_view(), name='blog-home'),  # class based view
+    path('user/<str:username>/', UserPostListView.as_view(), name='user-posts'),  # class based userPostList view
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),  # class based detail view
+    path('post/new/', PostCreateView.as_view(), name='post-create'),  # class based create view
+    path('post/<int:pk>/update', PostUpdateView.as_view(), name='post-update'),  # class based update view
+    path('post/<int:pk>/delete', PostDeleteView.as_view(), name='post-delete'),  # class based delete view
+    # path('', views.home, name='blog-home'), # function based view
+    path('about/', views.about, name='blog-about')
 ]
